749a579c37dc58a96b61a4b76e170c8978c3fc5b
Redhwan Qaid Mohammed ALASHRAM <radwan19922@gmail.com>
2026-08-19 00:13:56 +0300

    Update mikrotik.php

--- config/mikrotik.php
+++ config/mikrotik.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'host' => env('MIKROTIK_HOST', '192.168.88.1'),
-    'user' => env('MIKROTIK_USER', 'admin'),
-    'pass' => env('MIKROTIK_PASS', ''),
+    'host' => env('MIKROTIK_HOST', "9bd80a71f545.sn.mynetname.net"),
+    'user' => env('MIKROTIK_USER', 'mator'),
+    'pass' => env('MIKROTIK_PASS', 'mator122055'),
     'port' => env('MIKROTIK_PORT', 8728),
 ];
